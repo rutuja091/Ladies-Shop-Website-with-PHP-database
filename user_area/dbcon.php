81854701d7dc63ef2b19b2f84f4206c408ec9256
rutuja091 <gaikwadrutuja254@gmail.com>
2024-04-15 11:41:33 +0530

    add db con file

---
+++ user_area/dbcon.php
@@ -0,0 +1,9 @@
+<?php
+$databaseHost = "localhost";
+$databaseName = "ladiesshopdb";
+$databaseUsername = "root";
+$databasePassword = "";
+
+//Database connection 
+
+$con = new mysqli($databaseHost, $databaseUsername, $databasePassword,$databaseName)or die($conn->connect_error());
