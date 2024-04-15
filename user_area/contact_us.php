6ed3c63ab16a39bdc32032e60683bce5f177ceb3
rutuja091 <gaikwadrutuja254@gmail.com>
2024-04-15 11:29:50 +0530

    modify contact page

--- user_area/contact_us.php
+++ user_area/contact_us.php
@@ -37,13 +37,6 @@ font-size: 20px;
 font-family: 'Montserrat', sans-serif;
 }
 
-
-
-
-
-
-
-  
    /*start contact card css*/
 .contact_data1{
     background: #fff;
@@ -181,12 +174,6 @@ font-family: 'Montserrat', sans-serif;
 }
 /*end css social media icons hover effect*/
 
-/*end css for social media icons*/
-
- 
-
-
-  
  /* End contact  card css*/
 </style>
 </head>
@@ -223,17 +210,10 @@ font-family: 'Montserrat', sans-serif;
         <li class="nav-item">
           <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"></i><sup>1</sup></a>
         </li>
-       <!--<li class="nav-item">
-          <a class="nav-link" href="#">Total Price</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>-->
+      
+        
       </ul>
-      <!--<form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-light" type="submit">Search</button>
-      </form>-->
+     
     </div>
   </div>
 </nav>
